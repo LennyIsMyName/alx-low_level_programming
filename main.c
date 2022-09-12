f01d505872fe7ed5af58283ff97163140c18732b
LennyIsMyName <lennyofficial2001@gmail.com>
2022-09-12 10:57:01 -0700

    my first c program

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entrypoint
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+    int squared;
+
+    squared = getSquare(2);
+    printf("The value of squared is: %d\n")
+    return (0);
+
+}
