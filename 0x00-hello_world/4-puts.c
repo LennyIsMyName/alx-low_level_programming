7e6ad3bd77bcb9796e19d930fbb6603787611718
LennyIsMyName <lennyofficial2001@gmail.com>
2022-09-12 17:41:27 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ *
+ * print 'programming is like building a multilingular puzzle'
+ * Return: 0
+ */
+int main(void)
+{
+    puts("\"programming is like building a multilingular puzzle");
+    return (0);
+}
