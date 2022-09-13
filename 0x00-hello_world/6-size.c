6fc27f23a376684ef3c47c90b7673d15b46f1ad2
LennyIsMyName <lennyofficial2001@gmail.com>
2022-09-12 18:56:27 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the maing function
+ * print the size of each data type
+ * return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("size of a char: %ld byte(s)\n", sizeof(c));
+        printf("size of an int: %ld byte(s)\n", sizeof(i));
+        printf("size of a long int: %ld byte(s)\n", sizeof(li));
+        printf("size of a long long: %ld byte(s)\n", sizeof(lli));
+	return (0);
