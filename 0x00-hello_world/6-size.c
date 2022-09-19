c955c742d3ca9b7e4794daf7d3ed04a7445955de
LennyIsMyName <lennyofficial2001@gmail.com>
2022-09-19 03:12:00 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,8 +16,9 @@ int main(void)
 	float f;
 
 	printf("size of a char: %ld byte(s)\n", sizeof(c));
-        printf("size of an int: %ld byte(s)\n", sizeof(i));
-        printf("size of a long int: %ld byte(s)\n", sizeof(li));
-        printf("size of a long long: %ld byte(s)\n", sizeof(lli));
+	printf("size of an int: %ld byte(s)\n", sizeof(i));
+	printf("size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("size of a long long: %ld byte(s)\n", sizeof(lli));
+	printf("size of a float: %f byte(s)\n", sizeof(f));
 	return (0);
 }
