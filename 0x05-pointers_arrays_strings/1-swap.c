f4eecbdd6bc2af8659964caf40b48fb5f8caa61a
LennyIsMyName <lennyofficial2001@gmail.com>
2022-09-21 06:44:45 -0700

    strlen

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,11 +2,11 @@
 
 /**
  * swap_int - replaces 2 int values
- * a - first int
- * b _ secon int
+ * int a - first integer
+ * int b -  secon integer
  * Return: returns nothing
  */
-
+/*void swap_int - swap integers*/
 void swap_int(int *a, int *b)
 {
 	int tmp = *a;
