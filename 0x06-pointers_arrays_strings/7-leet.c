aff851e324928b3aab063630bf17543c3530b7fb
LennyIsMyName <lennyofficial2001@gmail.com>
2022-09-22 18:27:08 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,25 +2,25 @@
 
 /**
  * leet - encodes a string into 1337
- * @s: string to encode
+ * @n: string to encode
  *
  * Return: adress of s
  */
-char *leet(char *s)
+char *leet(char *n)
 {
 	int i, j;
 	char a[] = "aAeEoOtTlL";
 	char b[] = "4433007711";
 
-	for (i = 0; *(s + i); i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			for (j = 0; j <= 9; j++)
+			if (n[i] == a[j])
 			{
-				if (a[j] == *(s + i))
-					*(s + i) = b[j];
+				n[i] = b[j];
 			}
 		}
-		return (s);
+		return (n);
 	}
+}
