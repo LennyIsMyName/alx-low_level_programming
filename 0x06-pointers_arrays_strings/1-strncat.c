35a29a16ec37b80b6bad16b2ef593a2d27dfbe9d
LennyIsMyName <lennyofficial2001@gmail.com>
2022-09-22 17:42:36 -0700

    cat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,13 +11,18 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = strlen(dest), j = 0;
+	int i = 0, j = 0;
 
-	while (src[j] != '\0')
+	while (*(dest + i) != '\0')
+		i++;
+
+	while (*(src + j) != '\0' && j < n)
 	{
-		dest[i] = src[j];
+		*(dest + i) = *(src + j);
 		i++;
 		j++;
 	}
-	dest[i] = '\0';
+	*(dest + i) = '\0';
+
+	return (dest);
 }
