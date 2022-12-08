a71a97f1630e8cdb194e1121f5e7ad8fc56801a0
LennyIsMyName <lennyofficial2001@gmail.com>
2022-12-09 01:48:41 +0300

    ooh betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main- start point
- * Return: By default returns zero if no erros otherwise returns non-zero value.
+ * Return:returns zero if no erros otherwise returns non-zero value.
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
