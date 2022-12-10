5977f8e2172654939f71a2553ad43d1a7854ec36
LennyIsMyName <lennyofficial2001@gmail.com>
2022-12-10 04:17:39 +0300

    ommit some numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ * Return: always o
+ */
+
+int main(void)
+{
+	int l;
+
+		for (l = 'a'; l <= 'z'; l++)
+		{
+			if (l != 'q' || l != 'e')
+			putchar(l);
+		}
+		putchar('l');
+	return (0);
+}
