fb88a77349f911faefbbfdb0388975b2723eec61
LennyIsMyName <lennyofficial2001@gmail.com>
2022-12-11 00:46:48 +0300

    separate with commas

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,14 @@
 int main(void)
 {
 	int i;
+	int j = 9;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 9; i++)
 	{
 		putchar(i + '0');
 		putchar(',');
-		putchar(' ');
+	3	putchar(' ');
 	}
-	putchar(',');
+	putchar(j + '0');
 	return (0);
 }
