12505cb5fae249a6739b6ce617259ef3e2537bb9
LennyIsMyName <lennyofficial2001@gmail.com>
2022-12-12 08:03:04 +0300

    1-9 separated by commas and space

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,17 @@
 int main(void)
 {
 	int i;
-	int j = 9\n;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		putchar(i + '0');
+		if (i == 9)
+		{
+			putchar('\n');
+			return (0);
+		}
 		putchar(',');
 		putchar(' ');
 	}
-	putchar(j + '0');
 	return (0);
 }
