6dd1e5cd25c496082c5416151c195c585ba71048
LennyIsMyName <lennyofficial2001@gmail.com>
2022-12-14 13:07:19 +0300

    print alphas 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,18 +11,14 @@ int _putchar(char c);
 
 void print_alphabet_x10(void)
 {
-	int j;
 
-	for (j = 0; j <= 10; j++)
+	for (int j = 0; j <= 10; j++)
 	{
-
-	int i;
-
-	for (i = 'a'; i <= 'z'; i++)
+	for (int i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 		i++;
-		putchar('\n');
 	}
+
 	}
 }
