2847761ec8eba60fc71be91e23ea7d62b744ca78
LennyIsMyName <lennyofficial2001@gmail.com>
2022-12-14 20:21:27 +0300

    check sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,18 +6,23 @@
  * @n: the input
  * Return: 0 or 1
  */
+int _putchar(char c);
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (('+')1);
+		_putchar('+');
+		return (1);
 	}else if (n == 0)
 	{
-		return (('0')0);
+		
+		_putchar('0');
+		return (0);
 	}else if(n < 0)
 	{
-		return (('-')1);
+		_putchar('-');
+		return (1);
 	}
 	return (0);
 }
