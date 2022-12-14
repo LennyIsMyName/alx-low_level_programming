00829627a6b0452cd75c62e00238afe13128eddb
LennyIsMyName <lennyofficial2001@gmail.com>
2022-12-14 20:08:05 +0300

    check sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,14 +11,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return ('+', 1);
+		return ('+'1);
 		putchar('\n');
 	}else if (n == 0)
 	{
-		return ('0', 0);
+		return ('0'0);
 	}else if(n < 0)
 	{
-		return ('-', 1);
+		return ('-'1);
 	}
 	return (0);
 }
