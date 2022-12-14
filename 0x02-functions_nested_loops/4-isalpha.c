688c13c900ea63e2e5586ccb3822dfbfbcebf59e
LennyIsMyName <lennyofficial2001@gmail.com>
2022-12-14 17:40:14 +0300

    is it an alphabet?

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * _isalpha - checks wheather a character is an alphabet
+ * @c: the character to be tested
+ * Return: 1 if true, 0 if negative
+ */
+
 int _isalpha(int c)
 {
-	if ((c <= 65 && c >= 90) || (c <= 97 && c >= 122))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
 	return (0);
 }
