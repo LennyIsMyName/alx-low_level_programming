39eaab26b0b927dd847b933e3c9b847256cd635c
LennyIsMyName <lennyofficial2001@gmail.com>
2022-12-15 01:35:07 +0300

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+
+/**
+ * @i: the input
+ * print_last_digit - prints last digit
+ */
+
+int print_last_digit(int i)
+{
+	j = i % 10;
+	return (j);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 int _abs(int);
+int print_last_digit(int);
 int print_sign(int n);
 void print_alphabet_x10(void);
 int _putchar(char c);
