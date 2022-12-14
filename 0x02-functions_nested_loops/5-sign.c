cdb756096fc9e97a507701bf87c82129ef7be2d5
LennyIsMyName <lennyofficial2001@gmail.com>
2022-12-14 20:25:19 +0300

    check sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * 
  * print_sign - prints the sign
  * @n: the input
  * Return: 0 or 1
+ * @c: the input char
+ * _putchar - inputs the character
  */
 int _putchar(char c);
 
@@ -14,12 +15,11 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-	}else if (n == 0)
+	} else if (n == 0)
 	{
-		
 		_putchar('0');
 		return (0);
-	}else if(n < 0)
+	} else if (n < 0)
 	{
 		_putchar('-');
 		return (1);
