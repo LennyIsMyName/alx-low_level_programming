5344e923067c8269a84ea00b225e403571d43b88
LennyIsMyName <lennyofficial2001@gmail.com>
2022-12-18 12:14:56 +0300

    check if its a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,6 +4,7 @@
 /**
  * _isdigit - checks if its a digit
  * @c: the input
+ * Return: 0
  */
 
 int _isdigit(int c)
@@ -11,6 +12,7 @@ int _isdigit(int c)
 	int num = c;
 
 	int number = isdigit(num);
+
 	if (number)
 		return (1);
 	else
