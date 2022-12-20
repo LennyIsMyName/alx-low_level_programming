90e27339aec946d5c1d952f8de2af7f6639288f2
LennyIsMyName <lennyofficial2001@gmail.com>
2022-12-20 23:58:47 +0300

    copy str to str

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _strcpy - copies strings to strings
+ * @dest: copy to this
+ * @src: copy from this
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (0);
+}
