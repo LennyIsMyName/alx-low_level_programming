c6d4448a31c37e49dddcab31b652f4a88d329607
LennyIsMyName <lennyofficial2001@gmail.com>
2022-12-20 17:15:36 +0300

    print array to element n

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_array - print upto n
+ * @a: the array
+ * @n: element n
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+	int b;
+
+	b = a[n];
+	for (i = a[0]; i <= b; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+}
