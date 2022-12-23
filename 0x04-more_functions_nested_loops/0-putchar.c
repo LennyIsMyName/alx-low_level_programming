1f8eee7f3c645bc12fc664b77336df06cdc3b5e2
LennyIsMyName <lennyofficial2001@gmail.com>
2022-12-23 14:53:50 +0300

    print 0-9

---
+++ 0x04-more_functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main- main entry
+ * Return: always zero
+ * @c: the character to be printed
+ */
+int _putchar(char c);
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
