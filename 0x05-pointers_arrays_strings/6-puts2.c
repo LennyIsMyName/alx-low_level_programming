80bb4c003f78b7934597932bf869ee78a2dbc835
LennyIsMyName <lennyofficial2001@gmail.com>
2022-12-27 17:00:11 +0300

    print skipping odd-number indexes

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,43 +1,22 @@
 #include <string.h>
+#include <stdio.h>
+
 /**
  * puts2 - print skipping
  * @str: input
  * _putchar - print
- * @c: output
- * Return: nothing
  */
-
-int _putchar(char c);
 
 void puts2(char *str)
 {
 	int i;
-	int b;
-	
-	b = strlen(str);
-	i = 0;
+	int j;
 
-	for (i = str[0]; i <= b; i = i + 2)
+	j = strlen(str);
+	for (i = 0; i < j; i++)
 	{
-		int c = str[i];
-
-		_putchar(c);
-	}
-/*
-	while (i < b)
-	{
-		int a;
-
-		a = str[i];
 		if (i % 2 == 0)
-		{
-		_putchar(a);
-		}else if(i % 2 == 1)
-		{
-			i++;
-		}
+			putchar(str[i]);
 	}
-	*/
-	_putchar('\n');
+		putchar('\n');
 }
-
