f30447ab5aeb8f462bbba13d96a3bb55b8ed99dc
LennyIsMyName <lennyofficial2001@gmail.com>
2023-01-03 17:23:35 +0300

    copy n bytes from src to dest

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,12 @@
 #include <string.h>
 
+/**
+ * _memset - fills memory with a constant byte
+ * @s: the pointer to where to start
+ * @n: how many times
+ * @c: the const char to fill
+ * Return: s
+ */
 
 char *_memset(char *s, char c, unsigned int n)
 {
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include <string.h>
+
+/**
+ * _memcpy - copies bytes from src to dest
+ * @dest: destination
+ * @src: source
+ * @n: n times
+ * return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+		return (dest);
+}
