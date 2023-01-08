654bc879c8e411d4bd8a04b1e5c97f712d559db0
LennyIsMyName <lennyofficial2001@gmail.com>
2023-01-08 13:30:20 +0300

    strlen recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,6 +1,23 @@
 #include <string.h>
+/**
+ * _strlen_recursion - returns string length
+ * @s: string pointer
+ * Return: string length
+ */
 
-int _strlen_recursion(char *s)
+/*
+ * int _strlen_recursion(char *s)
 {
 	return(strlen(s));
 }
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 +_strlen_recursion(s + 1));
+	}
+	return (0);
+}
+
