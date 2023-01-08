df8ff748dfff6e27bc8ea0e4b3a77623e79eff1b
LennyIsMyName <lennyofficial2001@gmail.com>
2023-01-08 11:23:33 +0300

    strlen recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,7 +6,7 @@ int _putchar(char c);
 void _print_rev_recursion(char *s)
 {
 
-	if (*s != '\0') 
+	if (*s != s[0]) 
 		_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,6 @@
+#include <string.h>
+
+int _strlen_recursion(char *s)
+{
+	return(strlen(s));
+}
