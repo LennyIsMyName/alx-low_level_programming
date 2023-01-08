8abf9bcc0b0229d1c253c3a6d1eca559b7f97d74
LennyIsMyName <lennyofficial2001@gmail.com>
2023-01-08 13:33:58 +0300

    strlen recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,18 +5,11 @@
  * Return: string length
  */
 
-/*
- * int _strlen_recursion(char *s)
-{
-	return(strlen(s));
-}
-*/
-
 int _strlen_recursion(char *s)
 {
 	if (*s)
 	{
-		return (1 +_strlen_recursion(s + 1));
+		return (1 + _strlen_recursion(s + 1));
 	}
 	return (0);
 }
