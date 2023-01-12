d944d6f27f5e16ef26f71803c80c9339c59bf151
LennyIsMyName <lennyofficial2001@gmail.com>
2023-01-12 18:06:49 +0300

    print whatever is after the program name

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,14 +9,11 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc > 1)
-	{
-		int i;
+	int i;
 
-		for (i = 1; i < argc; i++)
-		{
+	for (i = 0; i < argc; i++)
+	{
 			printf("%s\n", argv[i]);
-		}
 	}
 	return (0);
 }
