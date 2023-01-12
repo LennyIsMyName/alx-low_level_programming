8acde730e7e7631d3e59a14118dd2f8f4d332b5e
LennyIsMyName <lennyofficial2001@gmail.com>
2023-01-12 17:37:51 +0300

    print no of args. argv parameter is unused.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: array of arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
