5ee0196f54921e7178f8a3d30e8f6791a2f5328e
LennyIsMyName <lennyofficial2001@gmail.com>
2023-01-13 14:13:12 +0300

    add all argv elements if they are numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - the main function
+ * @argc: the number of arguments
+ * @argv: the arguments
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	int i, num;
-	
+
 	num = 0;
 	if (argc == 1)
 	{
