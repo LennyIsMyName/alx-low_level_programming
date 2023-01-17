8ac8010a97a91a242e0dd8a23afab7a1ae8dcbf6
LennyIsMyName <lennyofficial2001@gmail.com>
2023-01-17 16:36:24 +0300

    make a copy of string passed as parameter.

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - makes a copy of a string passed as para..
+ * @str: the string
+ * Return: pointer to the copy or NULL
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int le, i;
+	char *c;
+
+	le = sizeof(str);
+	c = (char *) malloc(sizeof(char) * le);
+
+	if (!str)
+		return (NULL);
+
+	for (i = 0; i < le; i++)
+		c[i] = str[i];
+	return (c);
+}
