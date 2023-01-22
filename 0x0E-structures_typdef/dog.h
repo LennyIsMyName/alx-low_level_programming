5d817a2bee44f8e2cd286e4cb578ba21993f7cde
LennyIsMyName <lennyofficial2001@gmail.com>
2023-01-22 14:09:45 +0300

    define a struct in a .h

--- 0x0E-structures_typdef/dog.h
+++ 0x0E-structures_typdef/dog.h
@@ -1,12 +1,20 @@
 #ifndef _DOG_H
 #define _DOG_H
 
-struct dog
+/**
+ * struct dog - dog structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+struct 
 {
 	char *name;
 	float age;
 	char *owner;
 };
 
-
+typedef struct dog mi_dog;
+void asn_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* _DOG_H */
