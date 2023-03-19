9012d937a5b17a2edc9436865f828925d5a5124f
LennyIsMyName <lennyofficial2001@gmail.com>
2023-03-19 16:35:29 +0300

    add at start and end

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * free_dlistint - frees a linked list
+ * @head: pointer to the first node
+ */
+
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *curr, *next;
