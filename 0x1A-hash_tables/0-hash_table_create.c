7b04ce31578a40a34e7939175f6a69e37b2df78b
LennyIsMyName <lennyofficial2001@gmail.com>
2023-06-08 14:09:10 +0300

    correction

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t));
+	table->array = calloc(size, 8);
 
 	if (table->array == NULL)
 	{
